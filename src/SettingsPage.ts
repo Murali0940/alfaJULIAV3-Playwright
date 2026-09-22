3a944c42e39ca58d91c8088b959089b8fd33467b
Murali0940 <Murali0940@gmail.com>
2026-09-22 23:55:34 +0530

    settings page added

--- src/SettingsPage.ts
+++ src/SettingsPage.ts
@@ -3,15 +3,39 @@ import type { Page, Locator } from "@playwright/test";
 export class SettingsPage {
   readonly page: Page;
   readonly settingslink: Locator;
+  readonly materialAndThicknessLink: Locator;
+  readonly machineConfigurationlink: Locator;
+  readonly programConfigurationLink: Locator;
+  readonly gaiaConnectionLink: Locator;
+
 
   constructor(page: Page) {
     this.page = page;
     this.settingslink = page.getByTitle("Settings");
+    this.materialAndThicknessLink = page.getByRole('link', { name: 'Material / Thickness / Joint Master' });
+    this.machineConfigurationlink = page.getByRole('link', { name: 'Machine Configuration' });
+    this.programConfigurationLink = page.getByRole('link', { name: 'Program Configuration' });
+    this.gaiaConnectionLink = page.getByRole('link', { name: 'AI / GAIA Connection' });
+
   }
 
-    async clickSettingsLink() {
+  async clickSettingsLink() {
     await this.settingslink.click();
-    }
+  }
 
+  async clickMaterialAndThicknessLink() {
+    await this.materialAndThicknessLink.click();
+  }
 
+  async clickMachineConfigurationLink() {
+    await this.machineConfigurationlink.click();
+  }
+
+  async clickProgramConfigurationLink() {
+    await this.programConfigurationLink.click();
+  }
+
+  async clickGaiaConnectionLink() {
+    await this.gaiaConnectionLink.click();
+  }
 }
--- tests/alfa-julia-home.spec.ts
+++ tests/alfa-julia-home.spec.ts
@@ -5,7 +5,7 @@ import { SettingsPage } from "../src/SettingsPage.js";
 
 
 test.describe("alfaJULIA home page", () => {
-  test("Elements Validation", async ({ page }) => {
+  test("Homepage Elements Validation", async ({ page }) => {
     const homepageValidation = new HomepageValidation(page);
     await homepageValidation.selectLanguage("EN");
     await expect(homepageValidation.logo).toBeVisible();
@@ -21,9 +21,22 @@ test.describe("alfaJULIA home page", () => {
     await expect(homepageValidation.searchButton).toBeVisible();
     await homepageValidation.getrobotname();
     await homepageValidation.getCurrentRobotName();
+  });
 
+  test("settings Elements Validation", async ({ page }) => {
     const settingsPage = new SettingsPage(page);
     await settingsPage.clickSettingsLink();
     await expect(page).toHaveURL(/.*settings/);
+
+    await expect(settingsPage.materialAndThicknessLink).toBeVisible();
+    await expect(settingsPage.machineConfigurationlink).toBeVisible();
+    await expect(settingsPage.programConfigurationLink).toBeVisible();
+    await expect(settingsPage.gaiaConnectionLink).toBeVisible();
+
+
+    await settingsPage.clickMaterialAndThicknessLink();
+    await settingsPage.clickMachineConfigurationLink();
+    await settingsPage.clickProgramConfigurationLink();
+    await settingsPage.clickGaiaConnectionLink();
   });
 });
